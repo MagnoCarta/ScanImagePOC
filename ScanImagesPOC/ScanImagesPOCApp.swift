e123e4cec537dd1afa31314fc0b5566d8183f1be
MagnoCarta <gilbertomagno@10-45-50-100.0200.ifce.br.iosda.org>
2023-03-21 17:59:13 -0300

    First commit

---
+++ ScanImagesPOC/ScanImagesPOCApp.swift
@@ -0,0 +1,20 @@
+//
+//  ScanImagesPOCApp.swift
+//  ScanImagesPOC
+//
+//  Created by Gilberto Magno on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ScanImagesPOCApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
